31ddd1a36cb60636da84b7e4073cd96a339c6363
ilewis <idlewis@users.noreply.github.com>
2018-07-26 20:25:34 +0100

    OrderGroup is Data not MetaData

--- api/src/main/java/org/openmrs/OrderGroup.java
+++ api/src/main/java/org/openmrs/OrderGroup.java
@@ -21,7 +21,7 @@ import org.openmrs.api.APIException;
  * 
  * @since 1.12
  */
-public class OrderGroup extends BaseCustomizableMetadata<OrderGroupAttribute> {
+public class OrderGroup extends BaseCustomizableData<OrderGroupAttribute> {
 	
 	public static final long serialVersionUID = 72232L;
 	
